b901b38dbfc54690c8c5664ba6ce45c563e32452
kidupakon <kidupakon@gmail.com>
2018-05-18 13:39:07 +0700

    commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Mining Meter</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link type="text/css" rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<h1>This is a power consumption website</h1>
+
+	<fieldset>
+		<form action="./functions/actions.php" method="post">
+			<input type="text" name="date"> Date
+			<input type="text" name="reading">Reading
+			<input type="submit">
+		</form>
+	</fieldset>
+
+
+</body>
+</html>
+
+<?php 
+
+
+
+?>
